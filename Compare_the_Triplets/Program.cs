fe948c8a9befac03173139bd7d23a94492dfae9a
Krzysztof Bednarski <bednarski1978@gmail.com>
2018-11-04 10:56:03 +0100

    solution

---
+++ Compare_the_Triplets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare_the_Triplets
+{
+    class Program
+    {
+        static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            var aliceScore = (a[0] > b[0] ? 1 : 0) + (a[1] > b[1] ? 1 : 0) + (a[2] > b[2] ? 1 : 0);
+            var bobScore = (b[0] > a[0] ? 1 : 0) + (b[1] > a[1] ? 1 : 0) + (b[2] > a[2] ? 1 : 0);
+            return new List<int> { aliceScore, bobScore };
+        }
+        static void Main(string[] args)
+        {
+            var aliceList = new List<int>() { 5, 6, 7 };
+            var bobList = new List<int>() { 3, 6, 10 };
+           var result = compareTriplets(aliceList, bobList);
+
+            Console.WriteLine(String.Join(" ", result));
+            Console.ReadKey();
+        }
+    }
+}
